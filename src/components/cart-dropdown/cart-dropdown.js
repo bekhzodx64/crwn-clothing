52d267ebc9e4ba37ced2189140e0482aa2c33997
bekhzodx64 <mr.bekzod2016@mail.ru>
2022-06-15 17:25:03 +0500

    Project completed, Ready to deploy

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -14,9 +14,11 @@ const CartDropdown = () => {
 	return (
 		<div className='cart-dropdown-container'>
 			<div className='cart-items'>
-				{cartItems.map((item) => (
-					<CartItem key={item.id} cartItem={item} />
-				))}
+				{cartItems.length ? (
+					cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+				) : (
+					<span className='empty-message'>You cart is empty</span>
+				)}
 			</div>
 			<Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
 		</div>
--- src/routes/category/category.js
+++ src/routes/category/category.js
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card';
@@ -13,12 +13,17 @@ const Category = () => {
 	}, [category, categoriesMap]);
 
 	return (
-		<div className='category-page-container'>
-			{products &&
-				products.map((product) => (
-					<ProductCard key={product.id} product={product} />
-				))}
-		</div>
+		<Fragment>
+			<h2 className='text-[38px] text-center mb-[25px] uppercase'>
+				{category}
+			</h2>
+			<div className='category-page-container'>
+				{products &&
+					products.map((product) => (
+						<ProductCard key={product.id} product={product} />
+					))}
+			</div>
+		</Fragment>
 	);
 };
 
